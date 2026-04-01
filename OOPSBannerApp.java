1386cfab039810ebe3174866f16d87118b54c611
4zeemm <4zeemmm@gmail.com>
2026-04-01 18:46:05 +0530

    Add files via upload

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,70 @@
 public class OOPSBannerApp {
+
+    static class CharacterPattern {
+        private char character;
+        private String[] pattern;
+
+        public CharacterPattern(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        public char getCharacter() {
+            return character;
+        }
+
+        public String[] getPattern() {
+            return pattern;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        String[] patternO = {
+            " ***",
+            "* *",
+            "* *",
+            "* *",
+            "* *",
+            "* *",
+            " ***"
+        };
+
+        String[] patternP = {
+            "**** ",
+            "* *",
+            "* *",
+            "**** ",
+            "* ",
+            "* ",
+            "* "
+        };
+
+        String[] patternS = {
+            " ****",
+            "* ",
+            "* ",
+            " *** ",
+            "    *",
+            "    *",
+            "**** "
+        };
+
+        CharacterPattern[] bannerCharacters = {
+            new CharacterPattern('O', patternO),
+            new CharacterPattern('O', patternO),
+            new CharacterPattern('P', patternP),
+            new CharacterPattern('S', patternS)
+        };
+
+        for (int i = 0; i < 7; i++) {
+            StringBuilder lineBuilder = new StringBuilder();
+            for (int j = 0; j < bannerCharacters.length; j++) {
+                lineBuilder.append(bannerCharacters[j].getPattern()[i]);
+                if (j < bannerCharacters.length - 1) {
+                    lineBuilder.append("  ");
+                }
+            }
+            System.out.println(lineBuilder.toString());
+        }
     }
 }
